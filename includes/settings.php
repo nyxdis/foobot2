a0e27d497fae165eaa276b826f3eed8fd2c8df73
Christoph Mende <mende.christoph@gmail.com>
2011-01-09 10:11:32 +0100

    Introduced BOT_VERSION_ID and generated BOT_VERSION from that

--- includes/settings.php
+++ includes/settings.php
@@ -9,7 +9,7 @@
 /**
  * @ignore
  **/
-define('BOT_VERSION', '0.99.0');
+define('BOT_VERSION_ID', 9900);		// PHP bug? 009900 won't work
 define('LF', "\n");
 
 if ($argc != 2)
@@ -25,3 +25,9 @@ $required_settings[] = 'channels';
 foreach ($required_settings as $setting)
 	if (!isset ($settings[$setting]))
 		die ('Required setting \'' . $setting . '\' missing!');
+
+// Generate BOT_VERSION from BOT_VERSION_ID
+$major = floor(BOT_VERSION_ID / 10000);
+$minor = floor((BOT_VERSION_ID - ($major * 10000)) / 100);
+$micro = BOT_VERSION_ID - ($major * 10000) - ($minor * 100);
+define('BOT_VERSION', $major . '.' . $minor . '.' . $micro);
