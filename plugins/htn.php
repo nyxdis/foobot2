8ce3f06fec7e4d400a970a62542c971ddb4759f7
Christoph Mende <mende.christoph@gmail.com>
2011-01-10 19:20:21 +0100

    New plugin: htn

---
+++ plugins/htn.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * HackTheNet control plugin
+ *
+ * @author Christoph Mende <angelos@unkreativ.org>
+ * @package foobot
+ **/
+
+/**
+ * Implementation of plugin_interface
+ * @package foobot
+ * @subpackage plugins
+ **/
+class htn extends plugin_interface
+{
+	/**
+	 * Plugin initialization
+	 * @see plugins::register_event()
+	 **/
+	public function load()
+	{
+		$plugins = plugins::get_instance();
+
+		$plugins->register_event(__CLASS__, 'command', 'bug');
+	}
+
+	public function bug($args)
+	{
+		global $settings;
+
+		$id = (int)$args[0];
+		$url = 'http://hackthenet.org/_htn.php/bugtracker/showbug?output=json&id=' . $id;
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 20);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$result = curl_exec($ch);
+		$result = json_decode($result);
+		if(!$result) {
+			parent::answer('Bug nicht gefunden!');
+			return;
+		}
+		$response = 'http://hackthenet.org/_htn.php/bugtracker/showbug?id=' . (int)$result->id . ' "' . $result->subject . '"; state: ' . $result->state . '; user: ' . $result->user_name . '->' . $result->admin;
+		parent::answer($response);
+	}
+}
+
+?>
--- plugins/roulette.php
+++ plugins/roulette.php
@@ -78,7 +78,7 @@ class roulette extends plugin_interface
 			$nick = $usr->nick;
 		$nick = $db->quote($nick);
 		$cnt = $db->get_single_property('SELECT COUNT(`nick`) FROM `roulette` WHERE `nick` LIKE ' . $nick);
-		if($cnt > 0)
+		if ($cnt > 0)
 			$db->query('UPDATE `roulette` SET `' . $result . 's` = `' . $result . 's`+1 WHERE `nick` LIKE ' . $nick);
 		else
 			$db->query('INSERT INTO roulette (`nick`, `' . $result . 's`) VALUES(' . $nick . ', 1)');
@@ -137,7 +137,7 @@ class roulette extends plugin_interface
 
 		if (empty ($args)) {
 			$stats = $db->query('SELECT nick, (survivals * 100 / (survivals + deaths)) AS survival_rate FROM roulette WHERE (survivals + deaths) >= 50 ORDER BY survival_rate DESC LIMIT 5');
-			while($stat = $stats->fetchObject()) {
+			while ($stat = $stats->fetchObject()) {
 				$top[$stat->nick] = $stat->survival_rate;
 			}
 
@@ -147,7 +147,7 @@ class roulette extends plugin_interface
 			}
 
 			$i = 1;
-			foreach($top as $nick => $rate) {
+			foreach ($top as $nick => $rate) {
 				parent::answer('Top ' . $i . ': ' . $nick . ' (' . $rate . ' % survival rate)');
 				$i++;
 			}
