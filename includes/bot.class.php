fb92820fb1fec88505dcd941c685bfba751dc5e6
Christoph Mende <mende.christoph@gmail.com>
2011-01-10 09:54:25 +0100

    New plugin: definitions

--- includes/bot.class.php
+++ includes/bot.class.php
@@ -295,10 +295,7 @@ class bot
 			/* regular commands */
 			$plugins->run_event('command', $cmd, trim($args));
 
-			/* definitions */
-			if (preg_match('/(?<item>.+)(\?| is (?<definition>.+))/', $cmd . $args, $definfo)) {
-				$this->execute_command('define', $definfo);
-			} elseif (strncmp($channel, '#', 1) != 0) {
+			if (strncmp($channel, '#', 1) != 0) {
 				$this->say($settings['main_channel'], '<' . $cmdinfo['nick'] . '> ' . $cmd . ' ' . trim($args));
 			}
 		}
---
+++ plugins/definitions.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * definitions plugin
+ *
+ * @author Christoph Mende <angelos@unkreativ.org>
+ * @package foobot
+ **/
+
+/**
+ * Implementation of plugin_interface
+ * @package foobot
+ * @subpackage plugins
+ **/
+class definitions extends plugin_interface
+{
+	public function load()
+	{
+		global $settings;
+
+		$plugins = plugins::get_instance();
+
+		$trigger = '/' . $settings['command_char'] . '(?<item>.+)(\?| is (?<definition>.+))/';
+		$plugins->register_event(__CLASS__, 'text', $trigger, 'define');
+		$plugins->register_event(__CLASS__, 'command', 'forget');
+	}
+
+	public function define($args)
+	{
+		global $db;
+
+		$def = $db->query('SELECT * FROM `definitions` WHERE `item` LIKE ' . $db->quote($args['item']))->fetchObject();
+		if (isset ($args['definition'])) {
+			if (!$def)
+				$db->query('INSERT INTO `definitions` VALUES(' . $db->quote($args['item']) . ', ' . $db->quote($args['definition']) . ')');
+			else
+				$db->query('UPDATE `definitions` SET `description` = ' . $db->quote($args['definition']) . ' WHERE `item` LIKE ' . $db->quote($args['item']));
+			parent::answer('Okay.');
+			return;
+		}
+		if (!$def)
+			parent::answer($args['item'] . ' is undefined');
+		else
+			parent::answer($def->item . ' is ' . $def->description);
+	}
+
+	public function forget($args)
+	{
+		global $db;
+
+		if (empty ($args)) {
+			parent::answer('Forget what?');
+			return;
+		}
+		$db->query('DELETE FROM `definitions` WHERE `item` LIKE ' . $db->quote($args[0]));
+		self::answer('Okay');
+	}
+}
+
+?>
