769e91893e0bcfb77dee745da5bb4dedafd5a4e9
Christoph Mende <mende.christoph@gmail.com>
2011-01-11 11:04:49 +0100

    New plugin: fml

---
+++ plugins/fml.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Fuck My Life plugin
+ *
+ * @author Christoph Mende <angelos@unkreativ.org>
+ * @package foobot
+ **/
+
+/**
+ * Implementation of plugin_interface
+ * @package foobot
+ * @subpackage plugins
+ **/
+class fml extends plugin_interface
+{
+	public function load()
+	{
+		$plugins = plugins::get_instance();
+
+		$plugins->register_event(__CLASS__, 'command', 'fml', 'pub_fml');
+		$plugins->register_event(__CLASS__, 'command', 'fmsl');
+	}
+
+	public function pub_fml($args)
+	{
+                $url = 'http://www.fmylife.com/random';
+
+                $ch = curl_init();
+                curl_setopt($ch, CURLOPT_URL, $url);
+                curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 4);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+                $result = curl_exec($ch);
+                preg_match('/<a href="\S+" class="fmllink">(?<text>Today, .* FML)<\/a>/', $result, $fml);
+                $text = html_entity_decode(strip_tags($fml['text']));
+                parent::answer($text);
+	}
+
+	public function fmsl($args)
+	{
+		parent::answer('FMSL currently broken');
+	}
+}
+
+?>
