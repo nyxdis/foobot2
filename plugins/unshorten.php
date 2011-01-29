7036a3db3946012215756e876f1a7e3c9851714e
Christoph Mende <mende.christoph@gmail.com>
2011-01-29 17:50:31 +0100

    unshorten: minor usability fixes

--- plugins/unshorten.php
+++ plugins/unshorten.php
@@ -22,6 +22,9 @@ class unshorten extends plugin_interface
 	public function do_unshorten($args)
 	{
 		$short_url = trim($args['url']);
+		if (strncmp($short_url, 'http', 4) != 0)
+			$short_url = 'http://' . $short_url;
+
 		$ch = curl_init();
 		$url = 'http://www.unshorten.com/index.php';
 		curl_setopt($ch, CURLOPT_URL, $url);
@@ -33,9 +36,9 @@ class unshorten extends plugin_interface
 
 		$result = curl_exec($ch);
 		preg_match('/The real location of ' . preg_quote($short_url, '/') . ' is:\s+<br\/>\s+<a href="(?<real_url>[^"]+)/', $result, $matches);
-		if (urldecode($short_url) != urldecode($matches['real_url']) && !empty ($matches['real_url']) && !in_array($matches['real_url'], $ignore)) {
+		$real_url = str_replace(' ', '%20', $matches['real_url']);
+		if (urldecode($short_url) != urldecode($real_url) && !empty ($real_url) && !in_array($real_url, $ignore)) {
 			$bot = bot::get_instance();
-			$real_url = str_replace(' ', '%20', $matches['real_url']);
 			$bot->say($bot->channel, $short_url . ' => ' . $real_url);
 		}
 	}
