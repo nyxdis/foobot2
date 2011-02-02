ad10927aabc1f5dfc1eaa7037c4660e1abd2024d
Christoph Mende <mende.christoph@gmail.com>
2011-02-02 11:24:47 +0100

    sed: usability fixes

--- plugins/sed.php
+++ plugins/sed.php
@@ -29,19 +29,23 @@ class sed extends plugin_interface
 
 		$match = $args['match'] . $args['opts'];
 		if (!empty ($args['global']))
-			$ll = $this->lastline[$channel][0];
+			$ll = &$this->lastline[$channel][0];
 		else
-			$ll = $this->lastline[$channel][$usr->nick];
+			$ll = &$this->lastline[$channel][$usr->nick];
 		$ll = preg_replace($match, $args['replace'], $ll);
 		parent::answer($ll);
 	}
 
 	public function sed_save($args)
 	{
+		$text = implode(' ', $args);
+
+		if (preg_match('/^s(?<match>\/.*)\/(?<replace>.*)(?<opts>\/i?)(?<global>g?)/', $text))
+			return;
+
 		$usr = bot::get_instance()->usr;
 		$channel = bot::get_instance()->channel;
 
-		$text = implode(' ', $args);
 		$this->lastline[$channel][$usr->nick] = $text;
 		$this->lastline[$channel][0] = $text;
 	}
