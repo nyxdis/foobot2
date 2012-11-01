dbe367bbaac3f81b0d90cb7f7db78d3e50b35bb8
Christoph Mende <mende.christoph@gmail.com>
2012-11-02 00:04:05 +0100

    snackfixer: New plugin

---
+++ plugins/snackfixer.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * snack typo fixer
+ *
+ * @author Christoph Mende <mende.christoph@gmail.com>
+ * @package foobot
+ */
+
+/**
+ * Implementation of plugin_interface
+ * @package foobot
+ * @subpackage plugins
+ */
+class snackfixer extends plugin_interface
+{
+	/**
+	 * Plugin initialization
+	 * @see plugins::register_event()
+	 */
+	public function init()
+	{
+		$this->register_event('command', '/(' . preg_quote(settings::$command_char, '/') . ')?(?<gargabe>[snacmkl]{5})/', 'fix');
+	}
+
+	public function fix($args)
+	{
+		$str = $args[2];
+		if (substr_count($str, 's') == 1 &&
+			substr_count($str, 'a') == 1 &&
+			substr_count($str, 'c') == 1 &&
+			((substr_count($str, 'n') == 1 &&
+			substr_count($str, 'k') == 1) ||
+			(substr_count($str, 'm') == 1 &&
+			substr_count($str, 'l') == 1)))
+			plugins::run_event('command', 'q', array());
+	}
+}
+
+?>
